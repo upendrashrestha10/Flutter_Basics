53c0455ddd820c23f3296b35c86e345b34002501
upendrashrestha10 <shresthaupendra1234@gmail.com>
2025-08-11 22:11:02 +0545

    List view and Grid view

--- lib/List_Grid.dart
+++ lib/List_Grid.dart
@@ -48,6 +48,7 @@ class _ListGridState extends State<ListGrid> {
         ],
         ),
       */
+/* 
 //Now we are going to use listview.bulder
 //ListView.builder is a Flutter widget that efficiently creates a scrollable list by building items on demand using an itemBuilder callback, ideal for long or infinite lists to save memory.
       child: ListView.builder(
@@ -55,13 +56,56 @@ class _ListGridState extends State<ListGrid> {
         itemBuilder: (context, index) {
           return Card(
             child: ListTile(
+              onTap: () {
+                print((fruits_person['fruits'][index]));
+              },
               leading: Icon(Icons.person),
               title: Text(fruits_person['fruits'][index]),
               subtitle:Text(fruits_person['names'][index]) ,
             ),
           );
         },),
+*/
 
+//Gridview
+/* 
+      child: GridView(  //GridView in Flutter displays items in a scrollable 2D grid layout, useful for showing images or cards in rows and columns.
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount:2,crossAxisSpacing: 40, mainAxisSpacing: 40),
+        children: [
+          Card(
+            child: Center(child: Text("orange")),
+          ),
+
+          Card(
+            child: Center(child: Text("orange")),
+          ),
+
+          Card(
+            child: Center(child: Text("orange")),
+          ),
+
+          Card(
+            child: Center(child: Text("orange")),
+          ),
+
+          Card(
+            child: Center(child: Text("orange")),
+          ),
+        ],
+      ),
+*/
+
+//Gridview.builder = same as Listvie.builder
+      child: GridView.builder(
+        itemCount: fruits.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
+        itemBuilder: (context, index) {
+          return Card(
+            child: Center(child: Text(fruits[index])),
+          );
+        },
+      ),
       ),
     );
   }
