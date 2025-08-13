a2f902fd9d7441b84c549b1e9faf06078f8e2e24
upendrashrestha10 <shresthaupendra1234@gmail.com>
2025-08-13 21:16:17 +0545

    Snackbar

--- lib/Widget/Snackbar.dart
+++ lib/Widget/Snackbar.dart
@@ -10,8 +10,22 @@ class Snackbar extends StatelessWidget {
       body: Container(
         child: Center(
           child: ElevatedButton(
-            onPressed: (){}, 
-            child: Text('Show')),
+            onPressed: (){
+              final snackBar = SnackBar(
+                action: SnackBarAction(
+                  label:'Undo', 
+                  textColor: Colors.blue,
+                  onPressed: (){}),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(20)
+                ),
+                //for floating snackbar
+                behavior: SnackBarBehavior.floating,
+                duration: const Duration(seconds: 5),
+                content: Text("This is a Snackbar"));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }, 
+            child: Text('Show Snackbar')),
         ),
       ),
     );
